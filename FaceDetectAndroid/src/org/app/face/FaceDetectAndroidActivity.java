b909dc0d4e66fe2635236f3dd53d7270990a00d2
gabi-murgoci <gabriella.murgoci@gmail.com>
2014-05-22 21:46:08 +0300

    Added methods that open Camera or Gallery

--- FaceDetectAndroid/src/org/app/face/FaceDetectAndroidActivity.java
+++ FaceDetectAndroid/src/org/app/face/FaceDetectAndroidActivity.java
@@ -1,10 +1,19 @@
 package org.app.face;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class FaceDetectAndroidActivity extends Activity {
+
+	private static final int TAKE_PICTURE_CODE = 100;
+	public static final int GALLERY_REQUEST_CODE = 101;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -57,14 +66,64 @@ public class FaceDetectAndroidActivity extends Activity {
 		// obligatorie
 		super.onRestoreInstanceState(state);
 	}
-	
-	// what happens when "Choose a photo" is pressed
-	public void btnChooseAPhoto_onClick(View v)
-	{
+
+	public void btnRestart_onClick(View v) {
 		CustomizeDialog customizeDialog = new CustomizeDialog(this);
 		customizeDialog.show();
 	}
-	
-	
+
+	// ////////
+
+	private void openCamera() {
+		Intent intent = new Intent(
+				android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(intent, TAKE_PICTURE_CODE);
+	}
+
+	private void openGallery() {
+		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+		intent.setType("image/*");
+		startActivityForResult(intent, GALLERY_REQUEST_CODE);
+	}
+
+	// what happens when "Choose a photo" is pressed
+	public void btnChooseAPhoto_onClick(View v) {
+		CustomizeDialog customizeDialog = new CustomizeDialog(this);
+		customizeDialog.show();
+	}
+
+	// /////////////////////////////////////////////////////////
+	public class CustomizeDialog extends Dialog implements OnClickListener {
+		Button cancelButton;
+		Button takePhotoButton;
+		Button chooseLibraryButton;
+
+		public CustomizeDialog(Context context) {
+			super(context);
+			requestWindowFeature(Window.FEATURE_NO_TITLE);
+			setContentView(R.layout.custom_dialog_photo);
+
+			cancelButton = (Button) findViewById(R.id.btnCancel);
+			cancelButton.setOnClickListener(this);
+
+			takePhotoButton = (Button) findViewById(R.id.btnTakePhoto);
+			takePhotoButton.setOnClickListener(this);
+
+			chooseLibraryButton = (Button) findViewById(R.id.btnChoseLibrary);
+			chooseLibraryButton.setOnClickListener(this);
+		}
+
+		public void onClick(View v) {
+			if (v == cancelButton) {
+				dismiss();
+			} else if (v == takePhotoButton) {
+				openCamera();
+				dismiss();
+			} else if (v == chooseLibraryButton) {
+				openGallery();
+				dismiss();
+			}
+		}
+	}
 
 }
